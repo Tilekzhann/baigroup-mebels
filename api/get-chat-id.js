4d84394a825a700dec31424a6b64d6f47ac53705
Tilekzhann <tilekzh@gmail.com>
2025-12-18 01:02:37 +0500

    Update get-chat-id.js

--- api/get-chat-id.js
+++ api/get-chat-id.js
@@ -2,10 +2,3 @@
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot('8527285567:AAHruzB7JKIMf1JyiVQMnFOiBAcCXA1PkC8', {polling: true});
 
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  console.log(`Ваш Chat ID: ${chatId}`);
-  console.log(`Имя: ${msg.chat.first_name} ${msg.chat.last_name || ''}`);
-  bot.sendMessage(chatId, `Ваш Chat ID: ${chatId}`);
-  bot.stopPolling();
-});
